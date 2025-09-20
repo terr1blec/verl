0ef8ac9c85a8ceb89b91a739d74dccc0311fd92c
RolandMinrui <rolandminrui@gmail.com>
2025-09-20 17:01:25 +0800

    remove breakpoint()

--- verl/utils/reward_score/bfcl.py
+++ verl/utils/reward_score/bfcl.py
@@ -182,8 +182,6 @@ def compute_score(solution_str: str, ground_truth: str, extra_info=None) -> floa
             solution=extra_info['sol_final_config'],
             ground_truth=extra_info['gts_final_config'],
         )
-        if 0.5 * trace_score + 0.5 * answer_score > 0:
-            breakpoint()
         return 0.5 * trace_score + 0.5 * answer_score
         
     except Exception as e:
